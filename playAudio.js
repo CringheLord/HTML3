c3568ff9a55ceaa3ce3f186525a526d83d6dd606
CringheLord <v.grottola1@studenti.uniba.it>
2025-03-14 15:36:50 +0100

    commit

--- playAudio.js
+++ playAudio.js
@@ -1,6 +1,18 @@
-function playSound(soundId) {
+function playSoundAndNavigate(soundId, url) {
     var sound = document.getElementById(soundId);
     if (sound) {
-        sound.play();
+        sound.play().then(() => {
+            // Wait for the duration of the audio file before navigating
+            setTimeout(function() {
+                location.href = url;
+            }, sound.duration * 1000); // Delay navigation by the audio's duration (in milliseconds)
+        }).catch((error) => {
+            // Handle errors if audio doesn't play
+            console.error("Error playing sound: ", error);
+            location.href = url; // Navigate immediately if there's an error
+        });
+    } else {
+        // If sound element is not found, navigate immediately
+        location.href = url;
     }
 }
